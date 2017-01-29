69f8ce506b396006f1b88f1d92ee468a03ba856b
Demsem <demsem@mail.ru>
2017-01-29 12:41:06 +0300

    three commit

--- array.js
+++ array.js
@@ -1,14 +1,29 @@
 // JavaScript Document
 'use strict';
-var arr = ["Есть", "жизнь", "на", "Марсе"];
+var arr = new Array (10); 
+var min = 1;
+var max = 100;
 
-var arrLengths = arr.map(function(ar) {
-  return ar.length;
-});
-
-// получили массив с длинами
-alert( arrLengths ); 
-
-
-
-  
+for (var i=0;i<10;i++){
+ 	arr[i]=randomInteger(min,max);
+}
+	
+function randomInteger(min,max) {
+ 	var rand = min - 0.5 + Math.random() * (max - min + 1);
+ 	rand = Math.round(rand);
+ 	return rand;
+ }
+ 
+alert (GetSumm(arr));
+ 
+function GetSumm(arr){
+	var result = new Array (0);
+	var Total = new Array (0);
+	result = arr.reduce(function(sum, current){
+		Total.push(sum);
+		return sum + current;
+	});
+	
+	Total.push(result);
+	return Total;
+}
